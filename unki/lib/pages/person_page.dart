1e3c28dfba39f64d4d63d302ae26178b56a31185
marcos-inja <marcosvaas@gmail.com>
2021-08-20 14:51:42 -0300

    Cor da person page

--- unki/lib/pages/person_page.dart
+++ unki/lib/pages/person_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class PersonPage extends StatelessWidget {
   Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Color(0xff202125),
         body: Center(
           child: Text(
             'person.',
