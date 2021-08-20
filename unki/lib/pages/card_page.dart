339ef6b92c943a7076a8ecedea6e0e2711c2ec22
Jonathan Silva <jonathan.inja6@gmail.com>
2021-08-19 23:46:58 -0300

    pagina de cards iniciada

--- unki/lib/pages/card_page.dart
+++ unki/lib/pages/card_page.dart
@@ -1,20 +1,115 @@
 import 'package:flutter/material.dart';
-import 'package:unki/pages/task_page.dart';
+import 'task_page.dart';
 
 class CardPage extends StatelessWidget {
   Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            child: Text('Ver tarefas'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                
-                MaterialPageRoute(builder: (context) => TaskPage()),
-              );
-            },
+        appBar: AppBar(
+          title: Text('Pagina do Card'),
+          backgroundColor: Color(0xff54759E),
+        ),
+        body: _body(context),
+        backgroundColor: Color(0xff202125),
+      );
+}
+
+_body(context) {
+  return ListView(
+    children: [
+      MyStatelessWidget(),
+      editar(context),
+      iniciar(context),
+      buildContainerCard()
+    ],
+  );
+}
+
+MyStatelessWidget() {
+  return Column(
+    children: <Widget>[
+      Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Container(
+          color: Colors.white,
+          child: ListTile(
+            title: Text(
+              'Numbers',
+              textScaleFactor: 1.0,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              'To-do 10 Staks',
+              textScaleFactor: 1.1,
+              style: TextStyle(
+                height: 2,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ),
-      );
+      ),
+    ],
+  );
+}
 
+Widget editar(context) {
+  return TextButton(
+    style: TextButton.styleFrom(
+        backgroundColor: Color(0xff54759E),
+        elevation: 15,
+        shadowColor: Colors.green),
+    child: Text(
+      'Edite',
+      style: TextStyle(
+          color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+    ),
+    onPressed: () {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => MyStatelessWidget()));
+    },
+  );
+}
+
+Widget iniciar(context) {
+  return TextButton(
+    style: TextButton.styleFrom(
+        backgroundColor: Color(0xff54759E),
+        elevation: 15,
+        shadowColor: Colors.green),
+    child: Text(
+      'INICIAR',
+      style: TextStyle(
+          color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+    ),
+    onPressed: () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => TaskPage()));
+    },
+  );
+}
+
+Container buildContainerCard() {
+  return Container(
+    margin: EdgeInsets.all(16),
+    padding: EdgeInsets.symmetric(vertical: 100),
+    alignment: Alignment.center,
+    color: Colors.white,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Colocar algumas informações aqui!",
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    ),
+  );
 }
